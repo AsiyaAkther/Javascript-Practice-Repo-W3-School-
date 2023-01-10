ee1401a707bdd1750fa29eb2800c5e9e7f7636d9
AsiyaAkther <mahicse28@gmail.com>
2023-01-10 23:57:27 +0600

    Day seven

---
+++ js string method (replaceAll, toUpperCase, toLowerCase)/main.js
@@ -0,0 +1,9 @@
+function toUpperCase(){
+    let toUpperCaseText = document.getElementById("to_upper_demo").innerHTML;
+    document.getElementById("to_upper_demo").innerHTML = toUpperCaseText.toUpperCase();
+}
+
+function toLowerCase(){
+    let toLowerCaseText = document.getElementById("to_lower_demo").innerHTML;
+    document.getElementById("to_lower_demo").innerHTML = toLowerCaseText.toLowerCase();
+}
